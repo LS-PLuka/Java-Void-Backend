9dd393a11638346fedea052867818db7b37c50b7
Pedro Luka <pedrolukagt@gmail.com>
2025-11-16 18:58:39 -0300

    first commit

---
+++ src/main/java/com/example/demo/VoidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoidBackendApplication.class, args);
+	}
+
+}
