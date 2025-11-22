b75d3a11a9b1bf2054994506afbaccd4774a96dd
Pedro Luka <pedrolukagt@gmail.com>
2025-11-22 11:01:24 -0300

    feat: endpoints livres de usuarios

---
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.EditRequest;
+import com.example.demo.exceptions.UserNotFoundException;
+import com.example.demo.model.Usuario;
+import com.example.demo.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable Long id) {
+        usuarioService.deleteUsuario(id);
+        return ResponseEntity.ok(Map.of("message", "Usuário deletado com sucesso"));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> listUsuarios() {
+        return ResponseEntity.ok(usuarioService.listUsurios());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUsuarioById(@PathVariable Long id) {
+        Usuario usuario = usuarioService.getUsuarioById(id)
+                .orElseThrow(() -> new UserNotFoundException("Usuário não encontrado"));
+
+        return ResponseEntity.ok(usuario);
+    }
+}
